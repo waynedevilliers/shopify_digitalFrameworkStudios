3b1741c5ab6088aea2635bfdb129c48ecbc00119
Wayne de Villiers <wrdevilliers@gmail.com>
2025-04-03 00:04:34 +0200

    fix tailwind navbar issue

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./layout/**/*.liquid",
+    "./templates/**/*.liquid",
+    "./sections/**/*.liquid",
+    "./snippets/**/*.liquid",
+    "./assets/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
